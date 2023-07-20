942fb28b418f36f162aa725b7b068735da481903
Ruslan Karimov <ru-ka-@mail.ru>
2023-07-20 22:06:26 +0500

    added task_4: def booble sort

---
+++ seminar_4/task_4.py
@@ -0,0 +1,23 @@
+# Задание №4
+# ✔ Функция получает на вход список чисел.
+# ✔ Отсортируйте его элементы in place без использования
+# встроенных в язык сортировок.
+# ✔ Как вариант напишите сортировку пузырьком.
+# Её описание есть в википедии.
+
+list_nums = [10, 20, 33, 5, 66, 107, 44]
+
+
+def bubble_sort(nums_list: int) -> list[int]:
+    for i in range(len(nums_list) - 1):
+        for j in range(i + 1, len(nums_list)):
+            if nums_list[j] < nums_list[i]:
+                nums_list[j], nums_list[i] = nums_list[i], nums_list[j]
+
+    return nums_list
+
+
+print(bubble_sort(list_nums))
+# in place - на месте
+# если у функции есть окончание 'ed' (например, sorted) -> возвращает изменённую копию объекта
+# если нет, то функция мутирует текущий объект
