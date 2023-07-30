03b869a69bb9f5eccab76fe31e2dbf215f38dffb
Ruslan Karimov <ru-ka-@mail.ru>
2023-07-30 12:06:15 +0500

    edit task_2

--- seminar_7/task_2.py
+++ seminar_7/task_2.py
@@ -9,17 +9,31 @@
 import random
 
 
-def get_rnd_name():
-    vowels = "ауеиоуыэюя"
+def generate_name():
+    vowels = "аууееиоуыэюя"
     consonants = "бвгджзклмнпртсфчхшщ"
 
-    def gen_name(code):
-        return "".join((random.choice(vowels if char == '0' else consonants) for char in code))
+    def gen_type(code):
+        return "".join((random.choice(vowels if c == '0' else consonants) for c in code))
 
     codes = ['0', '1', '01', '10', '010', '101']
-    types = [gen_name(code) for code in codes]
+    types = [gen_type(code) for code in codes]
 
     return ''.join(random.sample(types, 3))
 
 
-print(get_rnd_name() if 4 < len(get_rnd_name()) < 7 else 'Неудачная попытка генерации')
+def validate_name():
+    while True:
+        name = generate_name()
+        if 4 < len(name) < 7:
+            break
+    return name
+
+
+def fill_file(file_name="names.txt", limit=10):
+    with open(file_name, mode="a", encoding="utf-8") as file:
+        for _ in range(limit):
+            file.write(validate_name().capitalize() + "\n")
+
+
+fill_file()
