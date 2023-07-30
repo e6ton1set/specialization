c3f39895d678c081b035124962c57a6ab6924274
Ruslan Karimov <ru-ka-@mail.ru>
2023-07-30 14:40:26 +0500

    update task_4: added def check_dir + create folder task_6 (test run def check_dir)

--- seminar_7/task_4.py
+++ seminar_7/task_4.py
@@ -17,18 +17,34 @@
 # ✔ Количество файлов для каждого расширения различно.
 # ✔ Внутри используйте вызов функции из прошлой задачи.
 
+# Задание №6
+# ✔ Дорабатываем функции из предыдущих задач.
+# ✔ Генерируйте файлы в указанную директорию — отдельный параметр функции.
+# ✔ Отсутствие/наличие директории не должно вызывать ошибок в работе функции
+# (добавьте проверки).
+# ✔ Существующие файлы не должны удаляться/изменяться в случае совпадения имён.
+
+
 from string import ascii_lowercase, digits
 from random import choices, randint
+from os import path
+import os
+
+
+def check_dir(dir, **kwargs) -> None:
+    if not path.exists(dir):
+        os.mkdir(dir)
+
+    os.chdir(dir)
+    make_exp(**kwargs)
 
 
 def make_files(exp: str, min_name_len: int = 6, max_name_len: int = 30,
                min_size: int = 256, max_size: int = 4096, amount: int = 42) -> None:
-
     for _ in range(amount):
-        name = ''.join(choices(ascii_lowercase+ digits + '_', k=randint(min_name_len, max_name_len)))
+        name = ''.join(choices(ascii_lowercase + digits + '_', k=randint(min_name_len, max_name_len)))
         data = bytes(randint(0, 255) for _ in range(randint(min_size, max_size)))
-
-        with open(rf'task_4\{name}.{exp}', mode='wb') as rnd_files:
+        with open(rf'{name}.{exp}', mode='wb') as rnd_files:
             rnd_files.write(data)
 
 
@@ -37,4 +53,4 @@ def make_exp(**kwargs):
         make_files(exp=exp, amount=amount)
 
 
-make_exp(txt=3, jpeg=2, doc=1)
+check_dir(r'.\\task_6', png=2, pdf=1)
