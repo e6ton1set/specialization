e27b2924030b17ee44cd4be38f65c5722e77a860
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-05 17:05:52 +0500

    upgrade home_1: added @deco_from_find_quadratic_roots

--- homework_9/home_1.py
+++ homework_9/home_1.py
@@ -10,18 +10,41 @@
 import cmath
 import csv
 import random
+from functools import wraps
+from typing import Callable
 
 SIZE = 100
 UPPER_LIMIT = 100
 LOWER_LIMIT = 1
 
 
-def find_quadratic_roots(a, b, c: any) -> None:
-    d = (b**2) - (4*a*c)
-    x1 = (-b-cmath.sqrt(d)) / (2*a)
-    x2 = (-b+cmath.sqrt(d)) / (2*a)
+def deco_from_find_quadratic_roots(func: Callable[[float], float]) -> Callable[[str], list[dict]]:
+    @wraps(func)
+    def wrapper(csv_file_path: str) -> list[dict]:
+        with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
 
-    print('Уравнение имеет корень {0} и {1}'.format(x1, x2))
+            result = []
+            for params in csv_reader:
+                if params != '':
+                    cur_result = {
+                        'params': params,
+                        'result': func(*params)
+                    }
+                result.append(cur_result)
+
+            return result
+
+    return wrapper
+
+
+@deco_from_find_quadratic_roots
+def find_quadratic_roots(a, b, c: any) -> tuple:
+    d = (b ** 2) - (4 * a * c)
+    x1 = (-b - cmath.sqrt(d)) / (2 * a)
+    x2 = (-b + cmath.sqrt(d)) / (2 * a)
+
+    return x1, x2
 
 
 def create_rnd_cvs(cvs_path: str) -> None:
@@ -33,10 +56,10 @@ def create_rnd_cvs(cvs_path: str) -> None:
         rows.append({'num1': num1, 'num2': num2, 'num3': num3})
 
     with open(cvs_path, 'w', encoding='utf-8') as csv_file:
-        three_rnd_nums = csv.DictWriter(csv_file, fieldnames=['num1', 'num2', 'num3'], dialect='excel-tab', restval=0)
+        three_rnd_nums = csv.DictWriter(csv_file, fieldnames=['num1', 'num2', 'num3'], dialect='excel')
         three_rnd_nums.writeheader()
         three_rnd_nums.writerows(rows)
 
 
-create_rnd_cvs('rnd_nums.cvs')
-
+if __name__ == '__main__':
+    print(deco_from_find_quadratic_roots("rnd_nums.csv"))
