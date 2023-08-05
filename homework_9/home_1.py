2efe1102776b8b79cf15e7a8e7ef6113526ae7a4
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-05 17:20:48 +0500

    update home_1: def gen_rnd_float_cvs

--- homework_9/home_1.py
+++ homework_9/home_1.py
@@ -3,6 +3,16 @@
 # ○ Генерация csv файла с тремя случайными числами в каждой строке (100-1000 строк).
 # ○ Декоратор, запускающий функцию нахождения корней квадратного уравнения с каждой тройкой чисел из csv файла.
 # ○ Декоратор, сохраняющий переданные параметры и результаты работы функции в json файл.
+import csv
+from random import randint, uniform
+
+
+def gen_rnd_float_cvs(file_path: str, min_row: int = 100, max_row: int = 1000,
+                      min_float: float = -100.0, max_float: float = 100) -> None:
+    with open(file_path, 'w', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file, dialect='excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+        for _ in range(randint(min_row, max_row)):
+            csv_writer.writerow(uniform(min_float, max_float) for _ in range(3))
 
 
 def find_quadratic_sqrt(a: float, b: float, c: float) -> tuple:
@@ -17,4 +27,4 @@ def find_quadratic_sqrt(a: float, b: float, c: float) -> tuple:
 
 
 if __name__ == '__main__':
-    print(find_quadratic_sqrt(2.2, -1.2, -6.5))
+    gen_rnd_float_cvs('rnd_nums.cvs')
