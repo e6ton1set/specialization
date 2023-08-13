0c7859e99a2d8e5c47abf328706478cfb157d248
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-13 19:16:03 +0500

    upgrade task_5: added parent Class Animal

--- seminar_10/task_5.py
+++ seminar_10/task_5.py
@@ -6,36 +6,47 @@
 # Для каждого класса создайте метод, выводящий
 # информацию специфичную для данного класса.
 
-class Fish:
-    def __init__(self, name: str, color: str, size: float, max_depth: float):
+# Задание №6
+# Доработайте задачу 5.
+# Вынесите общие свойства и методы классов в класс
+# Животное.
+# Остальные классы наследуйте от него.
+# Убедитесь, что в созданные ранее классы внесены правки.
+
+class Animal:
+    def __init__(self, name: str, color: str, size: float):
         self.name = name
         self.color = color
         self.size = size
+
+    def show_unique(self):
+        pass
+
+
+class Fish(Animal):
+    def __init__(self, name: str, color: str, size: float, max_depth: float):
+        super().__init__(name, color, size)
         self.max_depth = max_depth
 
-    def show_info(self):
+    def show_unique(self):
         print(f'Максимальная глубина: {self.max_depth}')
 
 
-class Bird:
+class Bird(Animal):
     def __init__(self, name: str, color: str, size: float, max_height: float):
-        self.name = name
-        self.color = color
-        self.size = size
+        super().__init__(name, color, size)
         self.max_height = max_height
 
-    def show_info(self):
+    def show_unique(self):
         print(f'Максимальная высота: {self.max_height}')
 
 
-class Cat:
-    def __init__(self, name: str, color: str, size: float, breed: str):
-        self.name = name
-        self.color = color
-        self.size = size
+class Cat(Animal):
+    def __init__(self, name: str, color: str, size: float, breed: float):
+        super().__init__(name, color, size)
         self.breed = breed
 
-    def show_info(self):
+    def show_unique(self):
         print(f'Порода: {self.breed}')
 
 
@@ -43,6 +54,8 @@ if __name__ == '__main__':
     fish_1 = Fish('Fish', 'Yellow', 3.3, 100)
     bird_1 = Bird('Bird', 'Red', 22.5, 500)
     cat_1 = Cat('Cat', 'Blue', 10.2, 'Myu')
-    fish_1.show_info()
-    bird_1.show_info()
-    cat_1.show_info()
+
+    animals = (fish_1, bird_1, cat_1)
+
+    for animal in animals:
+        animal.show_unique()
