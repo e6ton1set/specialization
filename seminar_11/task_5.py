f39a22e857fe1ebf2a67af6475ed4611decca6fb
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-17 20:48:17 +0500

    update task_5: methods __eq__, __gt__, __le__

--- seminar_11/task_5.py
+++ seminar_11/task_5.py
@@ -6,6 +6,11 @@
 # Складываем и вычитаем периметры, а не длинну и ширину.
 # При вычитании не допускайте отрицательных значений.
 
+# Задание №6
+# Доработайте прошлую задачу.
+# Добавьте сравнение прямоугольников по площади
+# Должны работать все шесть операций сравнения
+
 from seminar_10.task_2 import Rectangle
 
 
@@ -24,15 +29,25 @@ class RectanglePro(Rectangle):
         side_b = diff / 2 - side_a
         return RectanglePro(side_a, side_b)
 
+    def __eq__(self, other):
+        return self.get_square() == other.get_square()
+
+    def __gt__(self, other):
+        return self.get_square() > other.get_square()
+
+    def __le__(self, other):
+        return self.get_square() >= other.get_square()
+
 
 rect_1 = RectanglePro(2, 3)
 rect_2 = RectanglePro(5, 6)
-print(rect_1.get_perimeter())
-print(rect_2.get_perimeter())
-rect_sum = rect_1 + rect_2
-rect_sub = rect_1 - rect_2
-print(rect_sum.get_perimeter())
-print(rect_sub.get_perimeter())
-print(rect_sum.width, rect_sum.length)
-print(rect_sub.width, rect_sub.length)
-
+# print(rect_1.get_perimeter())
+# print(rect_2.get_perimeter())
+# rect_sum = rect_1 + rect_2
+# rect_sub = rect_1 - rect_2
+# print(rect_sum.get_perimeter())
+# print(rect_sub.get_perimeter())
+# print(rect_sum.width, rect_sum.length)
+# print(rect_sub.width, rect_sub.length)
+print(rect_1.get_square(), rect_2.get_square())
+print(rect_1 != rect_2)
