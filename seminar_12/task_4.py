164a2a9f5cf4c583ef4483f778c4216dfa9d0f42
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-21 21:28:06 +0500

    update task_4: added class Descriptor

--- seminar_12/task_4.py
+++ seminar_12/task_4.py
@@ -8,51 +8,52 @@
 # Доработаем прямоугольник и добавим экономию памяти
 # для хранения свойств экземпляра без словаря __dict__.
 
+# Задание №6
+# Изменяем класс прямоугольника.
+# Заменяем пару декораторов проверяющих длину и ширину
+# на дескриптор с валидацией размера
+
 import sys
+
+
+class Descriptor:
+    def __set_name__(self, owner, name):
+        self.name = '__' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if value > 0:
+            setattr(instance, self.name, value)
+        else:
+            raise ValueError(f'Значение {setattr(instance, value)} меньше 1')
+
 
 class Rectangle:
     """
     Rectangle class representation.
     """
-    __slots__ = ('__a', '__b')
+    # __slots__ = ('__a', '__b')
+
+    a = Descriptor()
+    b = Descriptor()
 
     def __init__(self, a: float, b: float = None):
         """
         Takes rectangle sides. Adds then to class instance.
         If only one provided, a square assumed.
         """
-        self.__a = a
-        self.__b = b if b is not None else a
-
-    @property
-    def a(self):
-        return self.__a
-
-    @a.setter
-    def a(self, value):
-        if value > 0:
-            self.__a == value
-        else:
-            raise ValueError(f'Значение {self.__a} меньше 1')
-
-    @property
-    def b(self):
-        return self.__b
-
-    @b.setter
-    def b(self, value):
-        if value > 0:
-            self.__b == value
-        else:
-            raise ValueError(f'Значение {self.__b} меньше 1')
+        self.a = a
+        self.b = b if b is not None else a
 
     def get_perimeter(self) -> float:
         """Returns perimeter (sum of all sides) of rectangle/square."""
-        return 2 * (self.__a + self.__b)
+        return 2 * (self.a + self.b)
 
     def get_area(self) -> float:
         """Returns area (product of two sides) of rectangle/square."""
-        return self.__a * self.__b
+        return self.a * self.b
 
     def __add__(self, other):
         """
@@ -61,7 +62,7 @@ class Rectangle:
         """
         sum_perimeter = self.get_perimeter() + other.get_perimeter()
         k = sum_perimeter / self.get_perimeter()
-        return Rectangle(self.__a * k, self.__b * k)
+        return Rectangle(self.a * k, self.b * k)
 
     def __sub__(self, other):
         """
@@ -70,7 +71,7 @@ class Rectangle:
         """
         sub_perimeter = abs(self.get_perimeter() - other.get_perimeter())
         k = sub_perimeter / self.get_perimeter()
-        return Rectangle(self.__a * k, self.__b * k)
+        return Rectangle(self.a * k, self.b * k)
 
     def __eq__(self, other):
         """Returns is Rectangle instance equal to another by its areas or not"""
@@ -85,13 +86,13 @@ class Rectangle:
         return self.get_area() <= other.get_area()
 
     def __repr__(self):
-        return f'Rectangle({self.__a}, {self.__b})'
+        return f'Rectangle({self.a}, {self.b})'
 
     def __str__(self):
-        if self.__a == self.__b:
-            return f'Square with side {self.__a}'
+        if self.a == self.b:
+            return f'Square with side {self.a}'
         else:
-            return f'Rectangle with sides {self.__a} and {self.__b}'
+            return f'Rectangle with sides {self.a} and {self.b}'
 
 
 if __name__ == '__main__':
@@ -99,5 +100,6 @@ if __name__ == '__main__':
     rect_2 = Rectangle(10, 20)
     # rect_1.b = 22
     # print(rect_1.b)
-    print(sys.getsizeof(rect_1)) # 48 байт вместо 56
-    print(dir(rect_1))
+    # print(sys.getsizeof(rect_1)) # 48 байт вместо 56
+    rect_2.a = 1
+    print(rect_2.a)
