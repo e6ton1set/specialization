c1de050d9196044f4f165fac048587f18c8f5c46
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-28 22:17:42 +0500

    upgrade task_2 -> task_3: added func sum_nums

---
+++ flask/seminar_1/task_3.py
@@ -0,0 +1,31 @@
+# Задание №3
+# Написать функцию, которая будет принимать на вход два
+# числа и выводить на экран их сумму.
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def print_hello_world():
+    return 'Hello World!'
+
+
+@app.route('/about/')
+def about():
+    return 'about'
+
+
+@app.route('/contact/')
+def contact():
+    return 'contact'
+
+
+@app.route('/<int:num_1>/<int:num_2>')
+def sum_nums(num_1: int, num_2: int) -> str:
+    return str(num_1 + num_2)
+
+if __name__ == '__main__':
+    app.run()
+
