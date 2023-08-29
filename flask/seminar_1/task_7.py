1b51260c1740f7e3d054afdfc3a966edc76921ba
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-29 23:01:30 +0500

    upgrade task_6 -> task_7: added route ./news, added page news.html

--- flask/seminar_1/task_7.py
+++ flask/seminar_1/task_7.py
@@ -6,7 +6,33 @@
 # Данные о новостях должны быть переданы в шаблон через
 # контекст.
 
+from datetime import datetime
+from flask import render_template
 from task_6 import app
+
+
+@app.route('/news/')
+def news():
+    news_block = [
+        {
+            'title': 'Новость первая',
+            'description': 'В г. Троицк Челябинской области прибыл известный актёр Сергей Безруков.',
+            'date_time': datetime.now().strftime('%H:%M - %m.%d.%Y года'),
+        },
+        {
+            'title': 'Новость вторая',
+            'description': 'Аномальные дожди!',
+            'date_time': datetime.now().strftime('%H:%M - %m.%d.%Y года'),
+        },
+        {
+            'title': 'Новость тетья',
+            'description': 'На фоне пандемии в 2020 году...',
+            'date_time': datetime.now().strftime('%H:%M - %m.%d.%Y года'),
+        }
+    ]
+
+    return render_template('news.html', news_block=news_block)
+
 
 if __name__ == '__main__':
     app.run()
