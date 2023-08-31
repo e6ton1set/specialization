30ae07a6771a03005364f2733f6b460c1ee2de5c
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-31 18:07:06 +0500

    added home_1: basic template and 2 child templates

---
+++ flask/homework_1/home_1.py
@@ -0,0 +1,32 @@
+# Урок 1. Знакомство с Flask
+# Задание
+#
+# Создать базовый шаблон для интернет-магазина,
+# содержащий общие элементы дизайна (шапка, меню, подвал),
+# и дочерние шаблоны для страниц категорий товаров и отдельных товаров.
+# Например, создать страницы «Одежда», «Обувь» и «Куртка», используя базовый шаблон.
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/vegetables/')
+def vegetables():
+    context = {'title': 'Vegetables'}
+    return render_template('vegetables.html', **context)
+
+
+@app.route('/fruits/')
+def fruits():
+    context = {'title': 'Fruits'}
+    return render_template('fruits.html', **context)
+
+
+if __name__ == '__main__':
+    app.run(port=5500)
