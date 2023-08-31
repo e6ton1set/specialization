45e8378d2cac0ec0811afde23c78b23c134dd43d
Ruslan Karimov <ru-ka-@mail.ru>
2023-08-31 16:10:36 +0500

    added folder optional_html: learn HTML

---
+++ optional_html/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sale/')
+def sale():
+    return render_template('sale.html')
+
+
+if __name__ == '__main__':
+    app.run()
