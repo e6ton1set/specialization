d6664ed0059ed2d5a40ec9b2f80446593b23e7b8
Ruslan Karimov <ru-ka-@mail.ru>
2023-09-21 18:19:35 +0500

    edit view 'index' -> index_html

--- django/homework/index_app/views.py
+++ django/homework/index_app/views.py
@@ -12,7 +12,7 @@ def index(request):
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Обо мне</title>
+            <title>Главная страница</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
                         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
                         crossorigin="anonymous">
@@ -21,13 +21,13 @@ def index(request):
                         crossorigin="anonymous"></script>
         </head>
         <body>
-            <div class="container text-center my-5">
+            <div class="container text-center my-5 ">
                 <h1>Главная страницa</h1>
             </div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container">
                     <a class="navbar-brand" href="/">
-                    <img src="https://clck.ru/35oYhP" alt="Ruslan K." width="55">
+                    <img src="https://clck.ru/35oYhP" alt="Ruslan K." width="100">
                     </a>
                     <button class="navbar-toggler" 
                     type="button" 
@@ -36,18 +36,28 @@ def index(request):
                     aria-controls="navbarNav" 
                     aria-expanded="false" 
                     aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                          <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="http://127.0.0.1:8000/about">Обо мне</a>
                             </li>
-
                         </ul>
                      </div>
                 </div>
             </nav>
+            <div class="card text-center cohabitation">
+                <div class="card-body">
+                    <h5 class="card-title">Привет!</h5>
+                    <p class="card-text">Это мой первый проект на фреймворке Django.</p>
+                </div>
+            </div>
+            <style>
+                .cohabitation { 
+                    margin-top: 50px; 
+                    }
+            </style>
         </body>
     </html>
     """
