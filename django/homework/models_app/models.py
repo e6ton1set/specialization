c05606c18b474febe0b990bd62f94f5cea0c8903
Ruslan Karimov <ru-ka-@mail.ru>
2023-09-24 21:03:02 +0500

    update homework_2: added models Client, Product and Order

--- django/homework/models_app/models.py
+++ django/homework/models_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=32)
+    email = models.EmailField()
+    phone = models.CharField(max_length=16)
+    address = models.CharField(max_length=128)
+    reg_date = models.DateField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default="Empty description")
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    amount = models.IntegerField(default=0)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateField(default=timezone.now())
