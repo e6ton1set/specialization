5b18e04958303e4dd0f80485f91e03eda310ea3b
Ruslan Karimov <ru-ka-@mail.ru>
2023-09-24 18:27:41 +0500

    edit project: added my command -> create_author and edit Models

---
+++ django/seminars/project/base_blog_app/management/commands/create_author.py
@@ -0,0 +1,24 @@
+from abc import ABC
+
+from django.core.management import BaseCommand
+from base_blog_app.models import Author, Article
+
+
+class Command(BaseCommand, ABC):
+    help = "Generate fake authors and articles"
+
+    def add_arguments(self, parser):
+        parser.add_argument("count", type=int, help="Count authors")
+
+    def handle(self, *args, **kwargs):
+        count = kwargs.get("count")
+        for i in range(1, count + 1):
+            author = Author(first_name=f"Name_{i}", email=f"mail_{i}@mail.ru")
+            author.save()
+            for j in range(1, count + 1):
+                article = Article(
+                    title=f"Title{j}",
+                    content=f"Text from {author.first_name} #_{j} bla-bla-bla",
+                    author=author
+                )
+                article.save()
--- django/seminars/project/base_blog_app/models.py
+++ django/seminars/project/base_blog_app/models.py
@@ -1,12 +1,13 @@
+import datetime as dt
 from django.db import models
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, default="None")
     email = models.EmailField()
-    biography = models.TextField()
-    birthday = models.DateTimeField()
+    biography = models.TextField(default="None")
+    birthday = models.DateField(default=dt.date.today())
 
     def get_full_name(self):
         full_name = f"{self.first_name} {self.last_name}"
@@ -20,8 +21,8 @@ class Article(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published = models.DateTimeField()
-    category = models.CharField(max_length=200)
+    published = models.DateField(default=dt.date.today())
+    category = models.CharField(max_length=200, default="category")
     show_count = models.IntegerField(default=0)
     is_published = models.BooleanField(default=False)
 
