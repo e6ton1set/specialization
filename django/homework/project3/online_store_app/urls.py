54af779c59f260b27dc78dbd55a2dcc85d631737
Ruslan Karimov <ru-ka-@mail.ru>
2023-10-04 13:59:56 +0500

    update online_store_app: added all_orders.html, route and view from all_orders.html

--- django/homework/project3/online_store_app/urls.py
+++ django/homework/project3/online_store_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("all_orders/<int:client_id>", views.all_orders, name="all_orders"),
 ]
--- django/homework/project3/online_store_app/views.py
+++ django/homework/project3/online_store_app/views.py
@@ -6,3 +6,24 @@ def index(request):
     clients = Client.objects.all()
     context = {"title": "Все клиенты", "clients": clients}
     return render(request, "online_store_app/index.html", context)
+
+
+def all_orders(request, client_id):
+    client = Client.objects.get(pk=client_id)
+    all_orders = {"unique_order": [], "products": []}
+    orders = Order.objects.all()
+    length_key = len(all_orders)
+
+    for order in orders:
+        if client_id == order.customer_id:
+            all_orders["unique_order"].append(order.id)
+
+    while length_key != 0:
+        curr_order = get_object_or_404(Order, pk=client_id)
+        products = Product.objects.filter(order=curr_order).all()
+        all_orders["products"].append(products)
+        length_key -= 1
+
+    context = {"all_orders": all_orders, "client": client, "order": curr_order}
+
+    return render(request, "online_store_app/all_orders.html", context)
