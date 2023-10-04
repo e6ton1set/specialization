6c2d3099869d31cd6ee02bfb9f84f54b4f98c4e1
Ruslan Karimov <ru-ka-@mail.ru>
2023-10-04 23:04:05 +0500

    update project3: rename 'base_project' -> 'baseproject'

--- django/homework/project1/base_project/urls.py
+++ django/homework/project1/base_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for base_project project2.
+URL configuration for baseproject project2.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
