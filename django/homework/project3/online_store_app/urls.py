6f16e8cbbecbaa9823fd16f5f474dc83b788c5d9
Ruslan Karimov <ru-ka-@mail.ru>
2023-10-04 14:48:21 +0500

    update online_store_app: added orders_by_time.html, route and view from orders_by_time.html

--- django/homework/project3/online_store_app/urls.py
+++ django/homework/project3/online_store_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("all_orders/<int:client_id>", views.all_orders, name="all_orders"),
+    path("orders_by_time/", views.orders_by_time, name="orders_by_time"),
 ]
--- django/homework/project3/online_store_app/views.py
+++ django/homework/project3/online_store_app/views.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
 from online_store_app.models import Client, Product, Order
 
 
@@ -27,3 +31,33 @@ def all_orders(request, client_id):
     context = {"all_orders": all_orders, "client": client, "order": curr_order}
 
     return render(request, "online_store_app/all_orders.html", context)
+
+
+def orders_by_time(request):
+    clients = Client.objects.all()
+    context = {
+        'clients': clients
+    }
+    if request.method == 'POST':
+        client_id = request.POST.get('client_id')
+        client = Client.objects.get(id=client_id)
+        days = request.POST.get('report')
+        if days == '7':
+            orders = Order.objects.filter(customer=client, date_ordered=timezone.now() - timedelta(days=7))
+            time_period = 'неделю'
+        elif days == '30':
+            orders = Order.objects.filter(customer=client, date_ordered=timezone.now() - timedelta(days=30))
+            time_period = 'месяц'
+        else:
+            orders = Order.objects.filter(customer=client, date_ordered=timezone.now() - timedelta(days=365))
+            time_period = 'год'
+        products = []
+        for order in orders:
+            products.extend(order.products.all())
+        products = list(set(products))
+        context.update({
+            'client': client,
+            'products': products,
+            'days': time_period
+        })
+    return render(request, 'online_store_app/orders_by_time.html', context)
